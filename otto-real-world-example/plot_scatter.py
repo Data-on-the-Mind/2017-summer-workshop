08db5434b65fade82307b2b0c69a3739ef21ef43
arossotto <ross.otto@gmaiol.com>
2017-06-28 08:54:36 -0700

    misc fixes...

--- otto-real-world-example/plot_scatter.py
+++ otto-real-world-example/plot_scatter.py
@@ -1,22 +1,19 @@
 from pylab import *
+from scipy.stats import *
 
 figure()
 [x, y] = [[],[]]
 grouped = lott.groupby('ZIP') 
 for zip, zip_lott in grouped:
     zip_x = demographics.ix[zip]['ses']
-
     zip_y = zip_lott.composite_per_capita.mean() 
-#    zip_y =  (zip_lott['composite_per_capita'].sum() / demographics.ix[zip]['income'])*100.
-
     x.append(zip_x)
     y.append(zip_y)
-
     text(zip_x, zip_y, str(zip))
     
     
 [m, b, r, p, std_err] = linregress(x,y)
 print 'r= '+str(r), 'p:', p
 plot( [min(x), max(x)], b + m*array([min(x), max(x)]))
-scatter(x,y) #,marker='+',s=30,color='black')
+scatter(x,y) 
 show()
